7db638199baaf7636ebfab85425da8de25bd4a0f
CharlesMich <charleedongo@hotmail.com>
2025-04-08 23:26:30 -0500

    Initial Commit

---
+++ personal_expense/personal_expenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  personal_expenseApp.swift
+//  personal_expense
+//
+//  Created by Charles Michael on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct personal_expenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
