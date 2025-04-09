76973b37e2b9f152547ccddf3f391f0d58d22941
CharlesMich <charleedongo@hotmail.com>
2025-04-08 23:59:33 -0500

    commit 2

---
+++ personal_expense/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  personal_expense
+//
+//  Created by Charles Michael on 4/8/25.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background1 = Color("Background")
+    static let icon1 = Color("Icon")
+    static let text1 = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
